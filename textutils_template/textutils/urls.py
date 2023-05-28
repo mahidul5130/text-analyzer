4e076116eedd2b8df251dbbc31a753bfb17cdb88
Md. Mahidul Haque <mahidul5130@gmail.com>
2023-05-28 14:13:53 +0600

    master

--- textutils_template/textutils/urls.py
+++ textutils_template/textutils/urls.py
@@ -1,3 +1,4 @@
+
 # Urls.py
 """textutils URL Configuration
 
@@ -18,8 +19,20 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+# Code for video 6
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.index, name='index'),
+#     path('about/', views.about, name='about'),
+#
+# ]
+
+# Code for video 7
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('analyze', views.analyze, name='analyze')
+    path('analyze', views.analyze, name='analyze'),
+    path('ex1', views.ex1, name='ex1'),
+
+
 ]
--- textutils_template/textutils/views.py
+++ textutils_template/textutils/views.py
@@ -10,6 +10,12 @@ def index(request):
     # return HttpResponse("Home")
 
 
+def ex1(request):
+    sites = ['''<h1>For Entertainment  </h1> <a href="https://www.youtube.com/"> Youtube Videos</a> ''',
+             '''<h1>For Interaction  </h1> <a href="https://www.facebook.com/"> Facebook</a> ''',
+             '''<h1>For Insight  </h1> <a href="https://www.ted.com/talks"> Ted Talks</a> ''',
+             '''<h1>For Internship  </h1> <a href="https://www.internshala.com">Internship</a> ''']
+    return HttpResponse((sites))
 
 def analyze(request):
     #Get the text
@@ -31,6 +37,34 @@ def analyze(request):
         params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', params)
 
+    elif(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+
+    elif (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
     else:
         return HttpResponse("Error")
 
