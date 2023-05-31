6c7355e57189fdc91eb1f6e8d3d07061f6e7c352
Md. Mahidul Haque <mahidul5130@gmail.com>
2023-05-31 11:23:50 +0600

    master

--- textutils_template/textutils/urls.py
+++ textutils_template/textutils/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
-    path('ex1', views.ex1, name='ex1'),
+    # path('ex1', views.ex1, name='ex1'),
 
 
 ]
