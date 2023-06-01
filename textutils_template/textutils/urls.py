010b5bad1a621e9b8621f6da152b287bc4b38c43
Md. Mahidul Haque <mahidul5130@gmail.com>
2023-06-01 19:36:38 +0600

    master

--- textutils_template/textutils/urls.py
+++ textutils_template/textutils/urls.py
@@ -1,5 +1,3 @@
-
-# Urls.py
 """textutils URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -19,20 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-# Code for video 6
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', views.index, name='index'),
-#     path('about/', views.about, name='about'),
-#
-# ]
-
-# Code for video 7
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
-    # path('ex1', views.ex1, name='ex1'),
-
+    path('about', views.about, name='about')
 
 ]
