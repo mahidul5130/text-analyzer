a0b744b467bac0ec6419b4115544cd281113c2a2
Md. Mahidul Haque <mahidul5130@gmail.com>
2023-06-02 20:08:32 +0600

    master

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -2,10 +2,8 @@ import string
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'index.html')
-
 
 def analyze(request):
     if request.method == 'POST':
@@ -15,25 +13,23 @@ def analyze(request):
         newlineremover = request.POST.get('newlineremover', False)
         extraspaceremover = request.POST.get('extraspaceremover', False)
         numberremover = request.POST.get('numberremover', False)
+        reversetext = request.POST.get('reversetext', False)
+        allspaceremover = request.POST.get('allspaceremover', False)
 
         analyzed = djtext
         params = {'purpose': '', 'analyzed_text': analyzed}
 
         if removepunc:
-            analyzed = ''.join(
-                char for char in analyzed if char not in string.punctuation)
-            params = {'purpose': 'Removed Punctuations',
-                      'analyzed_text': analyzed}
+            analyzed = ''.join(char for char in analyzed if char not in string.punctuation)
+            params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
 
         if fullcaps:
             analyzed = analyzed.upper()
-            params = {'purpose': 'Changed to Uppercase',
-                      'analyzed_text': analyzed}
+            params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
 
         if extraspaceremover:
             analyzed = ' '.join(analyzed.split())
-            params = {'purpose': 'Removed Extra Spaces',
-                      'analyzed_text': analyzed}
+            params = {'purpose': 'Removed Extra Spaces', 'analyzed_text': analyzed}
 
         if newlineremover:
             analyzed = analyzed.replace('\n', '').replace('\r', '')
@@ -43,13 +39,20 @@ def analyze(request):
             analyzed = ''.join(char for char in analyzed if not char.isdigit())
             params = {'purpose': 'Removed Numbers', 'analyzed_text': analyzed}
 
-        if not (removepunc or fullcaps or extraspaceremover or newlineremover or numberremover):
+        if reversetext:
+            analyzed = analyzed[::-1]
+            params = {'purpose': 'Reversed Text', 'analyzed_text': analyzed}
+
+        if allspaceremover:
+            analyzed = analyzed.replace(' ', '')
+            params = {'purpose': 'Removed All Spaces', 'analyzed_text': analyzed}
+
+        if not (removepunc or fullcaps or extraspaceremover or newlineremover or numberremover or reversetext or allspaceremover):
             return HttpResponse("Please select any operation and try again")
 
         return render(request, 'analyze.html', params)
     else:
         return HttpResponse("Invalid request method")
-
 
 def about(request):
     return render(request, 'about.html')
